53ed2fb14c00544016522e599614c8951b7ae731
tenpadrummer <yoshiro.kato@kate-lier.com>
2023-09-06 01:23:16 +0000

    books home rspec func complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#index'
+  resources :books, only: %i(index show create edit update destroy)
 end
